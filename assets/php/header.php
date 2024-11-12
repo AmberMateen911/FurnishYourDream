1bb8497ed52c9de3b197edae80e9b1f1295245fc
ambermateen <ambermatin911@gmail.com>
2024-11-12 12:27:34 +0500

    done

---
+++ assets/php/header.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="assets\css\header.css">
+</head>
+
+<body>
+
+    <header class="container flex-container">
+
+        <!--LOGO-->
+        <div class="menu-icon" onclick="toggleMenu(this)">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+        </div>
+
+        <div class="flex-container container divider">
+
+            <div id="logo">
+                <img src="assets\images\logo\logo.png" alt="logo">
+            </div>
+
+            <nav class="flex-container container " id="nav">
+                <ul class="flex-container">
+                    <li><a href="" class="active">HOME</a></li>
+                    <li><a href="">PRODUCT</a></li>
+                    <li><a href="">ABOUT US</a></li>
+                    <li><a href="">REGISTER</a></li>
+                </ul>
+            </nav>
+
+            <a href="" id="cart"><img src="assets\images\icons\basket.png" alt=""></a>
+
+        </div>
+    </header>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FYD</title>
+  <link type="image/png" href="assets\images\logo\logo.png" rel="icon">
+
+  <link rel="stylesheet" href="assets\css\global.css">
+  <link rel="stylesheet" href="assets\css\header.css">
+  <link rel="stylesheet" href="assets\css\style.css">
+  <link rel="stylesheet" href="assets\css\responsive.css">
+
+</head>
+
+<body>
+
+  <!--HEADRE-->
+  <?php include 'assets\php\header.php' ?>
+
+  <!---HERO SECTION-->
+  <section class="hero-banner">
+    <div class="content-divider flex-container">
+
+      <div class="circular-image">
+        <img src="assets\images\home\orange_sofaset.jpeg" alt="">
+      </div>
+
+      <div class="content-container  text-white ">
+        <h1 class="sans-serif ">FURNISH <br> YOUR DREAM</h1>
+        <p class="sans-serif font-16">At Dream Home Furnishing, we believe that every
+          home deserves furniture that combines style, quality, and comfort. Our collection
+          is crafted to inspire, helping you create the home of your dreams, one piece at a
+          time.</p>
+      </div>
+
+    </div>
+  </section>
+
+
+  <!--- TOP PRODUCT-->
+  <section class="container top-product-section">
+    <h2 class="text-white serif">Our Top Pick for You</h2>
+    <div class="row flex-container">
+      <!-- col-1 -->
+      <a href="" class="col">
+        <div class="tag">
+          <p class="text-white sans-serif font-12">BUY NOW</p>
+          <p class="text-white sans-serif font-16 offer">30% OFF</p>
+        </div>
+
+        <div class="image-container">
+          <img src="assets/images/home/sofa_chair.jpg" alt="">
+        </div>
+
+        <p class="text-white description">Create a platform where people can buy and sell pre-owned
+          furniture. This promotes sustainability and can appeal to
+          budget-conscious shoppers. promotes sustainability and can
+          appeal to budget-conscious shoppers. This promotes sustainability and can appeal to
+          budget-conscious shoppers. promotes sustainability and can
+          appeal to budget-conscious shoppers.
+        </p>
+      </a>
+
+      <!-- col-2 -->
+      <a href="" class="col">
+        <div class="tag">
+          <p class="text-white sans-serif font-12">BUY NOW</p>
+          <p class="text-white sans-serif font-16 offer">30% OFF</p>
+        </div>
+
+        <div class="image-container">
+          <img src="assets\images\home\yello_background.jpg" alt="">
+        </div>
+
+        <p class="text-white description">Create a platform where people can buy and sell pre-owned
+          furniture. This promotes sustainability and can appeal to
+          budget-conscious shoppers. promotes sustainability and can
+          appeal to budget-conscious shoppers. This promotes sustainability and can appeal to
+          budget-conscious shoppers. promotes sustainability and can
+          appeal to budget-conscious shoppers.
+        </p>
+      </a>
+
+      <!-- col-3 -->
+      <a href="" class="col">
+        <div class="tag">
+          <p class="text-white sans-serif font-12">BUY NOW</p>
+          <p class="text-white sans-serif font-16 offer">30% OFF</p>
+        </div>
+
+        <div class="image-container">
+          <img src="assets\images\home\orange_chair_2.png" alt="">
+        </div>
+
+        <p class="text-white description">Create a platform where people can buy and sell pre-owned
+          furniture. This promotes sustainability and can appeal to
+          budget-conscious shoppers. promotes sustainability and can
+          appeal to budget-conscious shoppers. This promotes sustainability and can appeal to
+          budget-conscious shoppers. promotes sustainability and can
+          appeal to budget-conscious shoppers.
+        </p>
+      </a>
+
+      <!-- col-4 -->
+      <a href="" class="col">
+        <div class="tag">
+          <p class="text-white sans-serif font-12">BUY NOW</p>
+          <p class="text-white sans-serif font-16 offer">30% OFF</p>
+        </div>
+
+        <div class="image-container">
+          <img src="assets\images\home\lamp.jpg" alt="">
+        </div>
+
+        <p class="text-white description">Create a platform where people can buy and sell pre-owned
+          furniture. This promotes sustainability and can appeal to
+          budget-conscious shoppers. promotes sustainability and can
+          appeal to budget-conscious shoppers. This promotes sustainability and can appeal to
+          budget-conscious shoppers. promotes sustainability and can
+          appeal to budget-conscious shoppers.
+        </p>
+      </a>
+
+
+    </div>
+    <div class="view-more">
+      <a href="" class=" text-white font-14 sans-serif">  VIEW MORE </a><span class="font-20 text-white bold">&gt;</span>
+    </div>
+
+<!---SIDE IMAGE-->
+    <div class="flex-container top-product-section ">
+      <div class="side-image-conatiner">
+        <img src="assets\images\home\enhance_brownsofa.PNG" alt="">
+      </div>
+      <div class="content"> 
+        <h2 class="text-white serif">Welcome to Furnish Your Dream</h2>
+        <p class="text-white serif">At Dream Home Furnishing, we believe that every home deserves furniture that 
+          combines style, quality, and comfort. Our collection is crafted to inspire, 
+          helping you create the home of your dreams, one piece at a time.</p>
+      </div>
+    </div>
+  </section>
+
+  <!---PRODUTC CATEGORY-->
+  
+
+</section>
+</body>
+
+</html>
